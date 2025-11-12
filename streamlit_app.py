d9a7bf9f17cf80f1c573baaf93914808c61096bd
parksuk1991 <148017439+parksuk1991@users.noreply.github.com>
2025-11-12 14:28:41 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -402,7 +402,7 @@ def main():
         with col2:
             st.markdown("""
             <div style="
-                height: 700px;
+                height: 680px;
                 display: flex;
                 align-items: center;
                 justify-content: center;
